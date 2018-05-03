909628373068cd7a428860e51c744305d5cfeb71
Soham Ghosh <ghosh.soham@gmail.com>
2018-05-03 00:37:24 -0400

    nothing works. values going to nan

--- NaturalParameterNetworks.py
+++ NaturalParameterNetworks.py
@@ -40,31 +40,10 @@ def gaussian_f_inv(m, s):
 def kappa(x):
     return torch.pow(1 + PI/8 * x, -0.5)
 
-class GaussianNPNCrossEntropy(autograd.Function):
-    """
-        Implements cross entropy for output layer
-
-        a_m is just used for forward. o_m, o_s used for loss
-    """
-    @staticmethod
-    def forward(ctx, a_m, o_m, o_s, y):
-        ctx.save_for_backward(o_m, o_s, y)
-        loss = torch.sum(torch.sum(torch.log(a_m) * y + torch.log(1 - a_m) * (1 - y), 1), 0)
-        return loss
-        
-    @staticmethod
-    def backward(ctx, grad_output):
-        o_m, o_s, y = ctx.saved_variables
-        grad__o_m = grad__o_s = None
-        grad__o_m = (torch.sigmoid(kappa(o_s) * o_m) - y) * kappa(o_s)
-        grad__o_s = (torch.sigmoid(kappa(o_s) * o_m) - y) * o_m * (- PI / 16.0 * torch.pow(1 + PI * o_s / 8.0 , -1.5))
-        return grad__o_m, grad__o_s, None, None
-
-
 class GaussianNPNKLDivergence(autograd.Function):
     """
         Implements KL Divergence loss for output layer
-        KL(N(o_c, o_d) || N(y_m, diag(eps))
+        KL(N(o_m, o_s) || N(y_m, diag(eps))
         https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Kullback%E2%80%93Leibler_divergence
     """
     @staticmethod
@@ -85,35 +64,27 @@ class GaussianNPNKLDivergence(autograd.Function):
 class GaussianNPNLinearLayer(nn.Module):
     def __init__(self, input_features, output_features):
         super(GaussianNPNLinearLayer, self).__init__()
-        self.weight_c, self.weight_d = self.get_init_W(input_features, output_features)
-        self.bias_c, self.bias_d = self.get_init_b(output_features)
+        self.weight_m, self.weight_s = self.get_init_W(input_features, output_features)
+        self.bias_m, self.bias_s = self.get_init_b(output_features)
 
     def get_init_W(self, _in, _out):
         # obtained from the original paper
         W_m = 2 * np.sqrt(6)/ np.sqrt(_in + _out) * (np.random.rand(_in, _out) - 0.5)
         W_s = 1 * np.sqrt(6)/ np.sqrt(_in + _out) * (np.random.rand(_in, _out))
-        W_c, W_d = gaussian_f_inv(W_m , W_s)
-        return nn.Parameter(torch.Tensor(W_c)), nn.Parameter(torch.Tensor(W_d))
+        return nn.Parameter(torch.FloatTensor(W_m)), nn.Parameter(torch.FloatTensor(W_s))
 
     def get_init_b(self, _out):
         b_m = np.zeros((_out))
         b_s = np.log(np.exp(-1 * np.ones((_out)) ) + 1)
-        b_c, b_d = gaussian_f_inv(b_m , b_s)
-        return nn.Parameter(torch.Tensor(b_c)), nn.Parameter(torch.Tensor(b_d))
+        return nn.Parameter(torch.Tensor(b_m)), nn.Parameter(torch.Tensor(b_s))
 
     def forward(self, input, activation=None):
         input_m, input_s = input
-        # input_m, input_s = gaussian_f(input_c, input_d)
-        weight_m, weight_s = gaussian_f(self.weight_c, self.weight_d)
-        bias_m, bias_s = gaussian_f(self.bias_c, self.bias_d)
-        o_m = torch.matmul(input_m, weight_m)
-        o_m += bias_m.unsqueeze(0).expand_as(o_m)
-        o_s = torch.matmul(input_s, weight_s) + \
-            torch.matmul(input_s, torch.pow(weight_m, 2)) + \
-            torch.matmul(torch.pow(input_m, 2), weight_s)
-        o_s += bias_s.unsqueeze(0).expand_as(o_s)
+        o_m = torch.addmm(self.bias_m, input_m, self.weight_m)
+        o_s = self.bias_s + torch.mm(input_s, self.weight_s) + \
+            torch.mm(input_s, torch.pow(self.weight_m, 2)) + \
+            torch.mm(torch.pow(input_m, 2), self.weight_s)
         return o_m, o_s
-
 
 
 class GaussianNPNNonLinearity(nn.Module):
@@ -150,6 +121,7 @@ class GaussianNPN(nn.Module):
             self.layers.append(GaussianNPNLinearLayer(input_features, output_classes))
         self.layers.append(GaussianNPNNonLinearity(activation))
         self.net = nn.Sequential(*list(self.layers)) # just to make model.parameters() work
+        self.lossfn = nn.BCELoss(size_average=False)
 
     def forward(self, x_m, x_s):
         return self.net((x_m, x_s))
@@ -158,10 +130,5 @@ class GaussianNPN(nn.Module):
         """
             output - torch.LongTensor
         """
-        out = input
-        for ix, L in enumerate(self.layers[:-1]):
-            out = L(out)
-        o_m, o_s = out
-        a_m, a_s = self.layers[-1]((o_m, o_s))
-        loss = GaussianNPNCrossEntropy.apply(a_m, o_m, o_s, y)
-        return loss
+        a_m, a_s = self.net(input)
+        return self.lossfn(a_m, y)
--- main.py
+++ main.py
@@ -7,16 +7,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 from torchvision import datasets
 from NaturalParameterNetworks import GaussianNPN
 
+SEED = 42  # my favorite seed
 
-torch.manual_seed(42) # my favorite seed
+torch.manual_seed(SEED)
+np.random.seed(SEED)
 
-def train(model, optimizer, epoch, batch_size=32, log_interval=100):
+def train(model, optimizer, epoch, batch_size=128, log_interval=10):
     model.train()
+    train_loss = 0
+    train_correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data = data.view(data.size(0), -1)
         data_m = Variable(data)
@@ -30,14 +35,23 @@ def train(model, optimizer, epoch, batch_size=32, log_interval=100):
         loss = model.loss((data_m, data_s), target_onehot)
         loss.backward()
         optimizer.step()
-        # exit()
+        train_loss += loss.data.numpy()[0]
+        
+        output = model(data_m, data_s)
+        pred = output[0].data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        train_correct += pred.eq(target.view_as(pred)).long().cpu().sum()
+
         if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
     
+    print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        train_loss / len(train_loader.dataset), train_correct, len(train_loader.dataset),
+        100. * train_correct / len(train_loader.dataset)))
+    
 
-def test(model, batch_size=32):
+def test(model, batch_size=128):
     model.eval()
     test_loss = 0
     correct = 0
@@ -50,6 +64,7 @@ def test(model, batch_size=32):
         target_onehot.scatter_(1, target.unsqueeze(1), 1)
         target_onehot = Variable(target_onehot)
         output = model(data_m, data_s)
+        
         loss = model.loss((data_m, data_s), target_onehot)
         pred = output[0].data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.view_as(pred)).long().cpu().sum()
@@ -68,18 +83,16 @@ if __name__ == '__main__':
         datasets.MNIST('./data', train=True, download=True,
                     transform=transforms.Compose([
                         transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
                     ])),
         batch_size=BATCH_SZ, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('./data', train=False, transform=transforms.Compose([
                         transforms.ToTensor(),
-                        transforms.Normalize((0.1307,), (0.3081,))
                     ])),
         batch_size=BATCH_SZ, shuffle=True)
 
-    model = GaussianNPN(784, 10, [128])
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    model = GaussianNPN(784, 10, [])
+    optimizer = optim.Adadelta(model.parameters())
 
     for epoch in range(1, 100 + 1):
         train(model, optimizer, epoch, batch_size=BATCH_SZ)
